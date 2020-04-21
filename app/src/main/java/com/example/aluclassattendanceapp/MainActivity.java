aea795c66c7cdca01a1930ba12950998c8667d4d
Lhet Magnan <lmagnan17@alustudent.com>
2020-04-21 16:50:27 +0200

    Final Work part 2 updated

--- app/src/main/java/com/example/aluclassattendanceapp/MainActivity.java
+++ app/src/main/java/com/example/aluclassattendanceapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.aluclassattendanceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.TextWatcher;
 import android.view.View;
@@ -15,8 +17,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText pref1,pref2;
-    Button button;
+    Student_dataBase myDatabase;
+    EditText pref1,pref2, pref3, pref4;
+    Button updateButton, deleteButton, saveButton, viewData;
     TextView studentName, password;
 
     public static final String MyPREFERENCES = "MyPrefs" ;
@@ -30,9 +33,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myDatabase = new Student_dataBase(this);
         pref1=(EditText)findViewById(R.id.studentName);
         pref2=(EditText)findViewById(R.id.password);
-
+        pref3=(EditText)findViewById(R.id.email);
+        pref4=(EditText)findViewById(R.id.studentID);
+        updateButton= (Button) findViewById(R.id.update);
+        deleteButton= findViewById(R.id.delete);
+        saveButton = findViewById(R.id.addButton);
+        viewData = findViewById(R.id.show);
+        update();
+        savingData();
+        delete();
+        viewDataSaved();
 
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         GetSavedData();
@@ -41,6 +54,87 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    public void update(){
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isUpdated = myDatabase.updatedata(pref1.getText().toString(),pref2.getText().toString(),pref3.getText().toString(),pref4.getText().toString());
+                if (isUpdated){
+                    Toast.makeText(MainActivity.this, "Data updated successfully", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Data not Updated", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+
+    public void delete(){
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer deleted = myDatabase.deletedata(pref4.getText().toString());
+                if(deleted<0){
+                    Toast.makeText(MainActivity.this,"Data Deleted successfully",Toast.LENGTH_LONG).show();
+
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Data not deleted recorded",Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+
+    public void savingData(){
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean result= myDatabase.adddata(pref1.getText().toString(),pref3.getText().toString(),pref2.getText().toString());
+                if(result){
+                    Toast.makeText(MainActivity.this,"Data recorded successfully",Toast.LENGTH_LONG).show();
+
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Data not successfully recorded",Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+
+    public void viewDataSaved(){
+        viewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer buffer = new StringBuffer();
+                Cursor res = myDatabase.showdata();
+                if (res.getCount()==0){
+                    showmessage("Error","no data");
+                    return;
+                }
+                while (res.moveToNext()){
+                    buffer.append("Id:"+res.getString(0)+"\n");
+                    buffer.append("name:"+res.getString(1)+"\n");
+                    buffer.append("email:"+res.getString(2)+"\n");
+                    buffer.append("password:"+res.getString(3)+"\n\n");
+
+                }
+                showmessage("Data",buffer.toString());
+            }
+        });
+    }
+
+    public void showmessage(String title,String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.show();
+    }
 
     private void saveData() {
         String StNa  = pref1.getText().toString();
